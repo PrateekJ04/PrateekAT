29868e2bb1b08b4361c026c1fe7fa531306f11d4
Prateek Joshi <jprat786@gmail.com>
2024-12-27 11:54:05 +0530

    Update common_verifications.py
    updates

--- src/helpers/common_verifications.py
+++ src/helpers/common_verifications.py
@@ -3,8 +3,8 @@ def verification_by_status_code(response_data, expected_data):
 
 
 def verification_json_key_notnull(key):
-    assert key != 0, "Failed - Key is not empty" + key
-    assert key > 0, "Failed - Key is greater than zero"
+    assert key != 0, "Failed - Key should not be empty" + key
+    assert key > 0, "Failed - Key should be greater than zero"
 
 def verify_key_is_not_none(key):
     assert key is not None
