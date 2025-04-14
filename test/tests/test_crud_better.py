8080bf2620a5e2ad4669205646a1961729b0d46c
Prateek Joshi <jprat786gmail.com>
2025-04-14 10:01:46 +0530

    generalized method

--- test/tests/test_crud_better.py
+++ test/tests/test_crud_better.py
@@ -18,9 +18,9 @@ class Test_Crud_Better(object):
     @pytest.mark.crud_better
     @allure.title("Verify if booking is getting updated")
     @allure.description("Details of Booking Id should get updated")
-    def test_put_booking(self, get_booking, get_token):
+    def test_put_booking(self, get_booking_id, get_token):
         response = apiwrapper.put_request(
-            url=Api_Constants.put_patch_delete_url(bookingid=get_booking),
+            url=Api_Constants.put_patch_delete_url(bookingid=get_booking_id),
             headers=readutils.common_headers_put_delete_cookie(
                 token=get_token),
             auth=None,
@@ -32,9 +32,9 @@ class Test_Crud_Better(object):
     @pytest.mark.crud_better
     @allure.title("Verify if booking is getting deleted")
     @allure.description("Details of Booking Id should get deleted")
-    def test_delete_booking(self, get_booking, get_token):
+    def test_delete_booking(self, get_booking_id, get_token):
         response = apiwrapper.delete_request(
-            url=Api_Constants.put_patch_delete_url(bookingid=get_booking),
+            url=Api_Constants.put_patch_delete_url(bookingid=get_booking_id),
             auth=None,
             headers=readutils.common_headers_put_delete_cookie(token=get_token),
             in_json=False)
